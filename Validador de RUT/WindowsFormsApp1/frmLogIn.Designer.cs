ca202457f7680edd9675906d22daa9abe91777fc
Insane23 <89926412+Insane23@users.noreply.github.com>
2022-05-04 11:41:54 -0400

    Archivos necesarios para ejecucion

---
+++ Validador de RUT/WindowsFormsApp1/frmLogIn.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmLogIn
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lblLogIn = new System.Windows.Forms.Label();
+            this.txtRut = new System.Windows.Forms.TextBox();
+            this.lblRut = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(142, 216);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(89, 44);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // lblLogIn
+            // 
+            this.lblLogIn.AutoSize = true;
+            this.lblLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogIn.Location = new System.Drawing.Point(138, 54);
+            this.lblLogIn.Name = "lblLogIn";
+            this.lblLogIn.Size = new System.Drawing.Size(137, 20);
+            this.lblLogIn.TabIndex = 1;
+            this.lblLogIn.Text = "Inicio de Sesion";
+            // 
+            // txtRut
+            // 
+            this.txtRut.Location = new System.Drawing.Point(142, 119);
+            this.txtRut.Name = "txtRut";
+            this.txtRut.Size = new System.Drawing.Size(193, 20);
+            this.txtRut.TabIndex = 2;
+            this.txtRut.Leave += new System.EventHandler(this.txtRut_Leave);
+            // 
+            // lblRut
+            // 
+            this.lblRut.AutoSize = true;
+            this.lblRut.Location = new System.Drawing.Point(51, 119);
+            this.lblRut.Name = "lblRut";
+            this.lblRut.Size = new System.Drawing.Size(24, 13);
+            this.lblRut.TabIndex = 3;
+            this.lblRut.Text = "Rut";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Location = new System.Drawing.Point(51, 172);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(61, 13);
+            this.lblContraseña.TabIndex = 4;
+            this.lblContraseña.Text = "Contraseña";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(142, 169);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.PasswordChar = '*';
+            this.txtContraseña.Size = new System.Drawing.Size(193, 20);
+            this.txtContraseña.TabIndex = 5;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(249, 216);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(86, 44);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // frmLogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 315);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.lblRut);
+            this.Controls.Add(this.txtRut);
+            this.Controls.Add(this.lblLogIn);
+            this.Controls.Add(this.btnAceptar);
+            this.Name = "frmLogIn";
+            this.Text = "LogIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lblLogIn;
+        private System.Windows.Forms.TextBox txtRut;
+        private System.Windows.Forms.Label lblRut;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ Validador de RUT/WindowsFormsApp1/frmLogIn.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmLogIn : Form
+    {
+        string rut = "19654048", pass = "1234qwer";
+        public frmLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if(txtRut.Text==rut && txtContraseña.Text == pass)
+            {
+                MessageBox.Show("Bueeena ctm");
+            }
+            else
+            {
+                MessageBox.Show("Te equivocaste zangano");
+                txtContraseña.Text = "";
+            }
+        }
+        private void ValidarRut()
+        {
+
+            int mult = 0, suma = 0, cont = 2;
+            //MessageBox.Show(numero);
+            string numero;
+            for (int i = txtRut.Text.Length - 2; i >= 0; i--)
+            {
+                numero = txtRut.Text.Substring(i, 1);
+                if (cont > 7)
+                {
+                    cont = 2;
+                }
+                mult = int.Parse(numero) * cont;
+                suma = suma + mult;
+                cont++;
+            }
+            MessageBox.Show("La suma es: " + suma);
+            string digito = "";
+            int resto = suma % 11, resta = 11 - resto;
+            if (resta == 10)
+            {
+                digito = "k";
+            }
+            else
+            {
+                if (resta == 11)
+                {
+                    digito = "0";
+                }
+                else
+                {
+                    digito = resta.ToString();
+                }
+            }
+            if (digito == txtRut.Text.ToLower().Substring(txtRut.Text.Length - 1))
+            {
+                MessageBox.Show("El rut es valido");
+            }
+            else
+            {
+                MessageBox.Show("RUT FALSO");
+            }
+        }
+        private void txtRut_Leave(object sender, EventArgs e)
+        {
+            
+            
+            if (txtRut.Text == "")
+            {
+                return;
+            }
+            ValidarRut();
+            
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtRut.Text = "";
+            txtContraseña.Text = "";
+        }
+    }
+}
